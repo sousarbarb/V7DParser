864133f7bed4018cf1c1e71b6236d9d5aab88493
Ricardo B. Sousa <sousa.ricardob@outlook.com>
2022-05-09 19:32:56 +0100

    Improve visualization computational performance results

--- src/evaluation/performance/visualize_results.m
+++ src/evaluation/performance/visualize_results.m
@@ -14,48 +14,70 @@ V7Dstc = V7DParser(N+1:end,:)';
 GLstc  = GerbLib(N+1:end,:)';
 
 %% LABELS
-labels = [];
+labels = cell(1,N);
+xaxislb = cell(N,1);
 for i=1:N
-  labels = [ labels ; repmat( sprintf("%d",i) , Ns , 1 ) ];
+  labels{i} = sprintf("%d",i);
+  if((i==1) || (floor(i/5) == ceil(i/5)))
+    xaxislb{i} = sprintf('%d',i);
+  else
+    xaxislb{i} = '';
+  end
 end
 
 %% VISUALIZATION
 figure
 hold on
-boxplot(V7Ddyn(:)/1000,labels)
+boxplot(V7Ddyn/1000,labels,'LabelOrientation','horizontal')
 grid on
+xlim([ 0 , N+1 ])
 ylim([ 0 , max([V7Ddyn(:)/1000 ; GLdyn(:)/1000]) ])
 xlabel('#D03 of a C standard aperture \rightarrow')
 ylabel('time (ms) \rightarrow')
 title('V7DParser')
 set(gcf, 'Position', [680 558 560 440])
+axisfig = gca;
+axisfig.XAxis.TickLabels = xaxislb;
+axisfig.XAxis.TickLabelRotation = 0;
 
 figure
 hold on
-boxplot(GLdyn(:)/1000,labels)
+boxplot(GLdyn/1000,labels)
 grid on
+xlim([ 0 , N+1 ])
 ylim([ 0 , max([V7Ddyn(:)/1000 ; GLdyn(:)/1000]) ])
 xlabel('#D03 of a C standard aperture \rightarrow')
 ylabel('time (ms) \rightarrow')
 title('GerbLib')
 set(gcf, 'Position', [680 558 560 440])
+axisfig = gca;
+axisfig.XAxis.TickLabels = xaxislb;
+axisfig.XAxis.TickLabelRotation = 0;
 
 figure
 hold on
-boxplot(V7Dstc(:)/1000,labels)
+boxplot(V7Dstc/1000,labels)
 grid on
+xlim([ 0 , N+1 ])
 ylim([ 0 , max([GLstc(:)/1000 ; V7Dstc(:)/1000]) ])
 xlabel('#D03 of a C standard aperture \rightarrow')
 ylabel('time (ms) \rightarrow')
 title('V7DParser')
 set(gcf, 'Position', [680 558 560 440])
+axisfig = gca;
+axisfig.XAxis.TickLabels = xaxislb;
+axisfig.XAxis.TickLabelRotation = 0;
 
 figure
 hold on
-boxplot(GLstc(:)/1000,labels)
+boxplot(GLstc/1000,labels)
 grid on
+xlim([ 0 , N+1 ])
 ylim([ 0 , max([GLstc(:)/1000 ; V7Dstc(:)/1000]) ])
 xlabel('#D03 of a C standard aperture \rightarrow')
 ylabel('time (ms) \rightarrow')
 title('GerbLib')
 set(gcf, 'Position', [680 558 560 440])
+axisfig = gca;
+axisfig.XAxis.TickLabels = xaxislb;
+axisfig.XAxis.TickLabelRotation = 0;
